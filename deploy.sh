7de2584ffb97b8b098b8faeb27ca22e478bcb3e7
zhiwen <lzw5399@gmail.com>
2020-04-24 16:08:25 +0800

    feat: setup vue project ci/cd

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# pull newest image
+sudo docker-compose pull >> /dev/null 2>&1
+if [[ $? != 0 ]]; then
+  exit 0
+fi
+
+# beaucuse we doesn't need build image in server, so doesn't need "--build"
+sudo docker-compose up --force-recreate -d >> /dev/null 2>&1
+if [[ $? != 0 ]]; then
+  exit 0
+fi
+
+# remove dangling images
+danglings=$(sudo docker images -f "dangling=true" -q)
+if test -n "$danglings"; then
+  sudo docker rmi $(sudo docker images -f "dangling=true" -q) >>/dev/null 2>&1
+  if [[ $? != 0 ]]; then
+    echo 'failed to remove danglings container...'
+    exit $?
+  fi
+fi
