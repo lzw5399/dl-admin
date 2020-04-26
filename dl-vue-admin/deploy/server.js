5c8a08dbe8c2359deb219436a9612a9c0583f667
zhiwen <lzw5399@gmail.com>
2020-04-26 21:30:04 +0800

    feat: implemented http cache in express level instead of nginx

--- dl-vue-admin/deploy/server.js
+++ dl-vue-admin/deploy/server.js
@@ -6,9 +6,14 @@ app.disable('x-powered-by')
 
 app.use(require('connect-history-api-fallback')())
 
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'public,max-age=315360000s')
+  next()
+})
+
 app.use(serveStatic('./dist'))
 
-const port = 80
+const port = 8090
 app.listen(port)
 
 console.log('server started ' + port)
