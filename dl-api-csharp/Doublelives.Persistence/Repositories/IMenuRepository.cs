e5bfb01e9fbfc069593ddc064ef060d752142eda
zhiwen <lzw5399@gmail.com>
2020-04-26 20:05:49 +0800

    chore: modify description

--- dl-api-csharp/Doublelives.Persistence/Repositories/IMenuRepository.cs
+++ dl-api-csharp/Doublelives.Persistence/Repositories/IMenuRepository.cs
@@ -5,10 +5,19 @@ namespace Doublelives.Persistence.Repositories
 {
     public interface IMenuRepository : IRepository<SysMenu>
     {
+        /// <summary>
+        /// 根据roleids获取去重后的权限url
+        /// </summary>
         List<string> GetPermissionsByRoleIds(List<int> roleIds, bool activeOnly = true);
 
+        /// <summary>
+        /// 根据roleids获取最顶层的菜单
+        /// </summary>
         List<SysMenu> GetTopLevelMenusByRoleIds(List<int> roleIds);
 
+        /// <summary>
+        /// 根据topmenu.Code获取它的下级菜单
+        /// </summary>
         List<SysMenu> GetSubMenusByParentCode(string id);
     }
 }
